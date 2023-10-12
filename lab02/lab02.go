92390f0e1d09a7ef17bdebf1ea465b69c643bccd
bosyuan <bosyuanhou@gmail.com>
2023-10-12 17:11:49 +0800

    pass lab02 test

--- lab02/lab02.go
+++ lab02/lab02.go
@@ -1,7 +1,8 @@
 package main
 
-import "fmt"
-
+import ( "fmt"
+		"strconv"
+)
 func main() {
 	var n int64
 
@@ -14,4 +15,18 @@ func main() {
 
 func Sum(n int64) string {
 	// TODO: Finish this function
+	var s string
+	sum := 0
+	for i := 1; i<= int(n); i++ {
+		if i%7 == 0 {
+			continue
+		}
+		s += strconv.Itoa(i) + "+"
+		sum += i
+	}
+	strAsBytes := []byte(s)
+	strAsBytes[len(strAsBytes)-1] = '='
+	s = string(strAsBytes)
+	s += strconv.Itoa(sum)
+	return s
 }
